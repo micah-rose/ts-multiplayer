174af3927bb95b4c556d250a7c002f0001c395ee
Micah Lund <micah.r.lund@gmail.com>
2022-01-11 22:16:33 -0700

    Completed lecture on interfaces.

--- foo.ts
+++ foo.ts
@@ -1,7 +1,16 @@
-function foo(bar: string) {
-    return "Hello, " + bar;
+//use interfaces to define your own types
+//the use of interfaces is when you are doing a merge
+//a merge is when the same interface is defined twice with a different parameter
+//TS treats keywords type and interface the same
+interface Quux {
+    quuz: string
+    corge: number
 }
 
-let baz = "ABC";
+function foo(bar: Quux) {
+    return "Hello, " + bar.quuz + " " + bar.corge;
+}
+
+let baz = {quuz: 'ABC', corge: 123}
 
 console.log(foo(baz));
