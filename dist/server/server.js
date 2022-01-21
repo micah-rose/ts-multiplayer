17aff5bf29d2ef227bf286493dcbaf986c783cf8
Micah Lund <micah.r.lund@gmail.com>
2022-01-20 17:22:04 -0700

    Ran the compiler again with the changes and got a js.map file with a script inside.

--- dist/server/server.js
+++ dist/server/server.js
@@ -2,21 +2,21 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var http_1 = __importDefault(require("http"));
-var socket_io_1 = require("socket.io");
-var port = 3000;
-var App = /** @class */ (function () {
-    function App(port) {
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const port = 3000;
+class App {
+    constructor(port) {
         this.port = port;
-        this.server = new http_1["default"].Server();
+        this.server = new http_1.default.Server();
         //const io: socketIO.Server = socketIO(this.server);
-        var io = new socket_io_1.Server(this.server);
+        const io = new socket_io_1.Server(this.server);
     }
-    App.prototype.Start = function () {
+    Start() {
         this.server.listen(this.port);
-        console.log("Server listening on port " + this.port);
-    };
-    return App;
-}());
+        console.log(`Server listening on port ${this.port}`);
+    }
+}
 new App(port).Start();
+//# sourceMappingURL=server.js.map
